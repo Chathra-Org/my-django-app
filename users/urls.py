007b12cf1f71503cff4543d3271a16e2338d84e3
Chathra Serasinghe <chathra@Chathras-MacBook-Pro.local>
2025-07-26 01:23:19 +0800

    removed delete user functionality

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from users import views
+
+urlpatterns = [
+    path('users/', views.add_user, name='add_user'),
+    path('users/<str:username>/', views.update_user, name='update_user'),
+    path('users/', views.list_users, name='list_users'),
+    path('users/<str:username>/', views.get_user, name='get_user'),
+   
+]
