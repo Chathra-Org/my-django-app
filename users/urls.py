27ee505c66242b306b814fdd48871204b9050acd
Chathra Serasinghe <chathra@Chathras-MacBook-Pro.local>
2025-07-26 08:23:41 +0800

    adding deleteuser function

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('users/<str:username>/', views.update_user, name='update_user'),
     path('users/', views.list_users, name='list_users'),
     path('users/<str:username>/', views.get_user, name='get_user'),
-    # Add more user-related URLs as needed
-    path('users/<str:username>/delete/', views.delete_user, name='delete_user'),
+    
     ]
 
 
