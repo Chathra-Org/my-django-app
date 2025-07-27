bdb1209709965e3a05f43767291499d07d29738b
chathra222 <chathraserasinghe89@gmail.com>
2025-07-27 15:57:34 +0800

    Update register view to handle error cases

--- registration/views.py
+++ registration/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .models import Registration
 
 def register(request):
-    message = None
+    errors = {}
     if request.method == "POST":
         first_name = request.POST.get("first_name")
         last_name = request.POST.get("last_name")
         email = request.POST.get("email")
-        if first_name and last_name and email:
+
+        if not first_name:
+            errors["first_name"] = "This field is required."
+        if not last_name:
+            errors["last_name"] = "This field is required."
+        if not email:
+            errors["email"] = "This field is required."
+        else:
+            try:
+                validate_email(email)
+            except ValidationError:
+                errors["email"] = "Enter a valid email address."
+            else:
+                if Registration.objects.filter(email=email).exists():
+                    errors["email"] = "A user with this email address already exists."
+
+        if not errors:
             Registration.objects.create(first_name=first_name, last_name=last_name, email=email)
-            message = "Registration successful!"
-    return render(request, "register.html", {"message": message})
+            return redirect("register_success")
+
+    return render(request, "register.html", {"errors": errors})
+
+def register_success(request):
+    return render(request, "register_success.html", {"message": "Registration successful!"})
 
 def registrations(request):
     regs = Registration.objects.all().order_by('-registered_at')
